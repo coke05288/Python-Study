2e449bfcd9cafdf79e83506fa4f8969e63cf3bc4
YoungJun <coke05288@gmail.com>
2020-09-16 14:58:48 +0900

    Programmers Brute-Force Find Prime Numbers

---
+++ Algorithm/Brute-Force/programmers_find_prime_number.py
@@ -0,0 +1,28 @@
+from itertools import permutations
+import math
+
+def isPrime(n):
+    k = int(math.sqrt(n))
+
+    if n <2:
+        return False
+
+    for i in range(2, k+1):
+        if n % i == 0 :
+            return False
+    return True
+    
+
+def solution(numbers):
+    answer = []
+
+    for i in range(1, len(numbers)+1):
+        num_list = list(map(''.join, permutations(list(numbers), i)))
+        for j in list(set(num_list)):
+            if isPrime(int(j)):
+                answer.append(int(j))
+
+    return len(set(answer))
+
+
+print(solution("17"))
